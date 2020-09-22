3e4b39c5a686ec11125c27a462533e3c28623e67
Muzyar Rad <muzyar.rad@gmail.com>
2020-09-22 11:19:49 -0700

    data is being received using fetch API and react hooks with async/await

--- src/card.js
+++ src/card.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 const Card = () => {
-  return <div></div>;
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getResults = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1", {
+        method: "GET",
+      });
+      const response = await res.json();
+      setData(response);
+    };
+    getResults();
+  }, []);
+  return <div>{console.log(data)}</div>;
 };
 
 export default Card;
